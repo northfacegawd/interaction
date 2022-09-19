9ca5ac48ccd9241010e7cf97b18f07498ed5602a
Lee yongjun <chuck5732590@gmail.com>
2022-09-19 20:44:38 +0900

    chore: weback babel 설정

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.ts",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.[contenthash:6].js",
+    clean: true,
+  },
+  devServer: {
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babe/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Interaction",
+      template: path.join(__dirname, "index.html"),
+    }),
+  ],
+};
